1ca6c2c29601bf9e1556445e628ba22829dcb2bf
Gregory <diazgregory@gmail.com>
2020-05-04 14:30:34 -0400

    webpack

---
+++ webpack.dev.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+}
