cd6ffc2610820a671b1e9b8ce398a8e605eaa5c5
Gregory <diazgregory@gmail.com>
2020-05-17 16:02:44 -0400

    change

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,32 +1,43 @@
 /* Global Variables */
 const entries = [];
 const picKey = "16477521-cf7894caf3d4932481ff6d4e3";
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 document.getElementById("generate").addEventListener('click',action);
 
-async function action (e) {
-    e.preventDefault();
-    e.stopPropagation();
+function formValue(){
     let country = document.getElementById("country").value;
     let zip = document.getElementById("zip").value;
     const endpoint = `http://api.geonames.org/postalCodeLookupJSON?postalcode=${zip}&country=${country}&username=magregor`
+    action(endpoint)
+}
+
+function createElements(value){
+    let createDiv = document.createElement("div");
+    createDiv.setAttribute("id","value");
+    var createhr = document.createElement("hr");
+    }
+
+async function action (endpoint) {
+    // e.preventDefault();
+    // e.stopPropagation();
+    formValue();
     const endpointweatherbi = `http://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=8ecd20f53453441b9064673acf586e0e`;
     const picendpoint = `https://pixabay.com/api/?key=${picKey}&q=yellow+flowers&image_type=photo`;
-    const data = await getData(endpoint);
+    var data = await getData(endpoint);
     Object.assign(data, { 
         newDate
     });
     const element = document.getElementById("entryHolder");
-    let createDiv = document.createElement("div");
-    createDiv.setAttribute("id","EntryInfo");
-    let createhr = document.createElement("hr");
+    var dataValue= data.getvalue(0);
+        createElements();
     data.postalcodes.forEach(info => {
         let createDivInter = document.createElement("div");
         createDivInter.appendChild(document.createTextNode(JSON.stringify(info,undefined,2)));
         createDiv.appendChild(createDivInter);
-        createDivInter.setAttribute("id",info.placeName);    
+        createDivInter.setAttribute("id",info.placeName); 
     });
         element.appendChild(createDiv);
         element.appendChild(createhr);
@@ -54,6 +65,9 @@ async function action (e) {
         });
         element.appendChild(createDivpic); 
 }
+
+export { action }
+
 const getData = async(endpoint) => {
     const res = await fetch(endpoint);
     try {
@@ -64,23 +78,3 @@ const getData = async(endpoint) => {
             console.log("error", error);
         }
 };
-
-// const postData = async(url ='/', data = {})=>{
-//     console.log(JSON.stringify(data));
-//     const res = await fetch(url,{
-//         method:'POST',
-//         credentials: 'same-origin',
-//         headers:{
-//             'Content-Type':'application/json',
-//         },
-//         body: JSON.stringify(data),
-//     });
-//     try {
-//         const newData = await res.json();
-//         console.log("Client" , newData);
-//         return newData;
-//     }
-// catch(error){
-//         console.log("error", error);
-//     }
-// };
